be006659dca1b3168d213cbd58593d806e86caed
GSZero <52993439+Lxkgit@users.noreply.github.com>
2026-09-03 17:38:20 +0800

    fix: remove project Steam proxy configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/tool/game/',
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/tool/game/steam-community': {
-        target: 'https://steamcommunity.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/tool\/game\/steam-community/, ''),
-      },
-    },
-  },
 })
