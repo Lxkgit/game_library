1a54fadd1a2d5bfab391cc953ac01c6791606de6
GSZero <52993439+Lxkgit@users.noreply.github.com>
2026-09-03 16:25:36 +0800

    feat: proxy Steam community requests in development

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/tool/game/',
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/steam-community': {
+        target: 'https://steamcommunity.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/steam-community/, ''),
+      },
+    },
+  },
 })
